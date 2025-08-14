f716078ac3cdb1ff994f61ed5607598e62d0f298
Sanwars02321 <santiagogabrielsegura@gmail.com>
2025-08-14 02:00:24 -0300

    feat: initial commit

---
+++ Assets/Programming/Scripts/PUNManager.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using TMPro;
+
+public class PUNManager : MonoBehaviourPunCallbacks
+{
+    private static PUNManager instance;
+
+    private void Awake()
+    {
+
+    }
+
+    public string roomName;
+    public TMP_InputField roomNameInputField;
+    public void ConnectToPUN()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public void CreateRoom()
+    {
+        PhotonNetwork.CreateRoom(roomName);
+        
+    }
+
+    public void setRoomName()
+    {
+        roomName = roomNameInputField.textComponent.text;
+    }
+
+    public void JoinRoom()
+    {
+      
+        if (string.IsNullOrEmpty(roomName))
+        {
+            Debug.Log("El nombre de la sala està vacio.");
+            return;
+        }
+        roomName = roomNameInputField.text;
+        PhotonNetwork.JoinRoom(roomName);
+    }
+
+    public void LeaveRoom()
+    {
+        PhotonNetwork.LeaveRoom();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("OnConnectedToMaster() was called by PUN.");
+        //PhotonNetwork.JoinRandomRoom();
+    }
+
+    public override void OnCreatedRoom()
+    {
+        base.OnCreatedRoom();
+        Debug.Log("Room created. Room name: "
+            + roomName);
+    }
+
+    public override void OnJoinedRoom()
+    {
+        base.OnJoinedRoom();
+        Debug.Log("Joined to room. Room name: "
+            + roomName);
+    }
+
+    public override void OnLeftRoom()
+    {
+        base.OnLeftRoom();
+        Debug.Log("Room left. Room name: "
+           + roomName);
+    }
+}
